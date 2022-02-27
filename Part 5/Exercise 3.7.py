9c4520cacc3de62a2d42026468460d55c33cbdf3
triciappa <98954224+triciappa@users.noreply.github.com>
2022-02-27 13:42:04 +0100

    Create Exercise 3.7.py

---
+++ Part 5/Exercise 3.7.py
@@ -0,0 +1,33 @@
+# Write a function that returns a dictionary which have as its keys the numbers from 0 to 99 
+# and the value attached to each key should be the number spelled out in words.
+
+def dict_of_numbers():
+    numbers = {}
+    d = { 0 : 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five',
+          6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine', 10 : 'ten',
+          11 : 'eleven', 12 : 'twelve', 13 : 'thirteen', 14 : 'fourteen',
+          15 : 'fifteen', 16 : 'sixteen', 17 : 'seventeen', 18 : 'eighteen',
+          19 : 'nineteen', 20 : 'twenty',
+          30 : 'thirty', 40 : 'forty', 50 : 'fifty', 60 : 'sixty',
+          70 : 'seventy', 80 : 'eighty', 90 : 'ninety' }
+    
+    for i in range (0,100,1):
+        if (i < 20):
+            numbers[i] = d[i]
+        
+        elif (i < 100):
+            if i % 10 == 0: 
+                numbers[i] = d[i]
+            else: 
+                numbers[i] = d[i // 10 * 10] + '-' + d[i % 10]
+
+        i += 1
+    return numbers
+
+if __name__ == "__main__":
+    numbers = dict_of_numbers()
+    print(numbers[2])
+    print(numbers[11])
+    print(numbers[45])
+    print(numbers[99])
+    print(numbers[0])
